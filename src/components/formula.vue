ad6684222dec318091ed48c02b0c07ee81f33aa8
tongzhirong <tongzhirong@bizvane.cn>
2021-12-10 18:32:31 +0800

    公式

--- src/components/formula.vue
+++ src/components/formula.vue
@@ -2,16 +2,22 @@
     <div class="formula">
         <div class="content" ref="content" @keydown.left="handleKeyDown" @click.prevent="handleClickContent">
             <template v-for="(item, index) in tokens">
-                <span :key="index" v-if="item.type === 1" class="is-box">
+                <span :key="index" v-if="item.type === 1" class="is-box" :dataIndex="index" :dataType="item.type">
                     {{ item.value }}
                 </span>
-                <span v-if="item.type === 2" :key="index" class="is-string">
+                <span v-if="item.type === 2" :key="index" class="is-string" :dataIndex="index" :dataType="item.type">
                     {{ item.value }}
                 </span>
             </template>
         </div>
         <div class="cursor" ref="cursor"></div>
-        <input v-model="inputVal" ref="input" @blur="handleBlur" @input="handleInput" @keyup="handleKeyUp" />
+        <input
+            v-model="inputVal"
+            ref="input"
+            @blur="handleBlur"
+            @input="(val) => handleInput(val)"
+            @keyup="handleKeyUp"
+        />
     </div>
 </template>
 <script>
@@ -23,116 +29,151 @@
                     {
                         value: '日期',
                         type: 1,
+                        start: 0,
+                        end: 2,
                     },
                     {
                         value: '1',
                         type: 2,
+                        start: 2,
+                        end: 3,
                     },
                     {
                         value: '2',
                         type: 2,
+                        start: 3,
+                        end: 4,
+                    },
+                    {
+                        value: '姓名',
+                        type: 1,
+                        start: 5,
+                        end: 7,
                     },
                 ],
-                inputVal: '1 + 2',
+                inputVal: '',
+                cursorIndex: null,
             }
-        },
-        // watch: {
-        //     inputVal(newVal) {
-        //         if (newVal) {
-        //             this.tokens = parse(newVal).map((item) => {
-        //                 const { start, end } = item
-        //                 return {
-        //                     ...item,
-        //                     value: end === -1 ? newVal.substring(start) : newVal.substring(start, end),
-        //                 }
-        //             })
-        //         }
-        //     },
-        // },
-        mounted() {
-            // const { inputVal } = this
-            // this.tokens = parse(inputVal).map((item) => {
-            //     const { start, end } = item
-            //     return {
-            //         ...item,
-            //         value: end === -1 ? inputVal.substring(start) : inputVal.substring(start, end),
-            //     }
-            // })
         },
         methods: {
             // 点击div的时候处理焦点
-            handleClickContent() {
+            handleClickContent(e) {
+                this.cursorIndex = e.target.getAttribute('dataindex')
+                console.log(e.target.getAttribute('dataindex'), this.cursorIndex)
                 let selection = window.getSelection()
                 let range = selection.getRangeAt(0)
                 const { startContainer, endContainer, startOffset } = range
-                console.log('range1', startContainer.start)
+                console.log('startOffset', range, startOffset)
                 let startNode = startContainer
                 let endNode = endContainer
                 // 拿文本节点的父节点
-                if (startNode.parentNode._prevClass === 'is-box' && endNode.parentNode._prevClass === 'is-box') {
+                if (startNode.nodeType === 3) {
                     startNode = startNode.parentNode
-                    range.selectNode(endNode.parentNode)
-                    range.collapse(true)
-                    console.log('range12', range)
                 }
-                console.log('hah', startNode.start)
-                let start = startOffset + startNode.start
-                let input = this.$refs.input
-                // 设置input选中文本的起始和结束位置
-                input.setSelectionRange(start, start)
-
-                console.log('result', result)
+                if (endNode.nodeType === 3) {
+                    endNode = endNode.parentNode
+                }
+                range.selectNode(startNode)
                 let result = range.getBoundingClientRect()
-                const { left } = result
-                input.style.left = `${left}px`
-                // 真实光标
-                input.focus()
-                // 设置虚拟光标的位置
-                this.$refs.cursor.style.left = `${left}px`
+                // range.collapse(false)
+                let input = this.$refs.input
+                console.log('reqult', e.target.getAttribute('datatype'))
+                if (e.target.getAttribute('datatype') === '1') {
+                    const { x } = result
+                    input.style.left = `${42 + x}px`
+                    input.focus()
+                    this.$refs.cursor.style.left = `${42 + x}px`
+                } else {
+                    const { x } = result
+                    input.style.left = `${x}px`
+                    input.focus()
+                    this.$refs.cursor.style.left = `${x}px`
+                }
                 this.$refs.cursor.style.visibility = 'visible'
-                // // 如果content没有值
-                // if (!this.inputVal) {
-                //     this.$refs.cursor.style.height = `15px`
-                //     this.$refs.cursor.style.left = `5px`
-                // }
             },
             // input失去焦点时隐藏光标
             handleBlur() {
                 this.$refs.cursor.style.visibility = 'hidden'
             },
-            handleInput() {
-                //   this.updateRange();
+            handleInput(val) {
+                this.updateRange(val.data)
             },
-            handleKeyUp(e) {
-                console.log('e', e)
+            handleKeyUp() {
+                // console.log('e', e)
             },
-            updateRange() {
-                let input = this.$refs.input
-                let start = input.selectionStart
-                let end = input.selectionEnd
-                let i = this.tokens.findIndex((item) => {
-                    return end <= item.end
+            updateRange(val) {
+                const { end } = this.tokens[this.cursorIndex]
+                this.tokens.splice(this.cursorIndex, 0, {
+                    type: 2,
+                    value: val,
+                    start: end,
+                    end: end + 1,
                 })
-                console.log('i', start, end)
-                let node = this.$refs.content.children[i]
-                console.log('hahah', this.$refs.content.children)
-                let content = node.firstChild
-                if (content.nodeType !== 3) {
-                    content = node.querySelector('[data-content]')
-                    content = content.firstChild
+                this.tokens.forEach((item, index) => {
+                    if (index === 0) {
+                        item = {
+                            ...item,
+                            start: 0,
+                            end: item.value.length,
+                        }
+                    } else {
+                        const { end } = this.tokens[index - 1]
+                        item = {
+                            ...item,
+                            start: end,
+                            end: end + item.value.length,
+                        }
+                    }
+                })
+
+                let realNode = []
+                let childNodes = this.$refs.content.childNodes
+                let len = childNodes.length
+                console.log('childNodes', childNodes)
+                for (let i = 0; i <= len - 1; i++) {
+                    console.log('i', i)
+                    if (childNodes[i].nodeType === 1) {
+                        console.log(i, childNodes[i])
+                        realNode.push(childNodes[i])
+                    }
                 }
-                let selection = window.getSelection()
-                let range = selection.getRangeAt(0)
-                range.setStart(content, end - node.start)
-                range.collapse(true)
-                let result = range.getBoundingClientRect()
-                const { left, height } = result
-                this.$refs.cursor.style.left = `${left}px`
-                this.$refs.cursor.style.height = `${height}px`
+
+                console.log('this.$refs.content', realNode)
+                // let selection = window.getSelection()
+                // let range = selection.getRangeAt(0)
+                // range.selectNode(this.$refs.content.children[this.cursorIndex + 1])
+                // let result = range.getBoundingClientRect()
+                // console.log('result', result)
+                // let input = this.$refs.input
+                // let cursorStart = input.selectionStart
+                // let cursorEnd = input.selectionEnd
+                console.log('cursorStart', this.$refs.content)
+                // let node = this.$refs.content.children[start]
+                // console.log('hahah', this.$refs.content.children)
+                // let content = node.firstChild
+                // if (content.nodeType !== 3) {
+                //     content = node.querySelector('[data-content]')
+                //     content = content.firstChild
+                // }
+                // // 最后一个
+                // let last = this.$refs.content.children[this.$refs.content.children.length - 1]
+                // let selection = window.getSelection()
+                // let range = selection.getRangeAt(0)
+                // range.setStart(last, 0)
+                // range.collapse(true)
+                // let result = range.getBoundingClientRect()
+                // console.log('result', last)
+                // const { left } = result
+                // input.style.left = `${left}px`
+                // input.focus()
+                // console.log(input)
+                // let result = range.getBoundingClientRect()
+                // const { left } = result
+                // this.$refs.cursor.style.left = `${left}px`
                 this.$refs.cursor.style.visibility = 'visible'
             },
-            handleKeyDown(e) {
-                console.log(e.keyCode)
+            handleKeyDown() {
+                // console.log(e.keyCode)
             },
         },
     }
@@ -169,13 +210,14 @@
             background-color: black;
             top: 4px;
             left: 5px;
+            visibility: hidden;
         }
         input {
             position: absolute;
-            top: 0;
-            left: 0;
+            top: 4px;
+            left: 5px;
             width: 1px;
-            height: 1px;
+            height: 40px;
             opacity: 0;
             padding: 0px;
             border-width: 0px;
